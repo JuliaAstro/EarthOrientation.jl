b6d07a548bc247ecc857c046a0f7018ff6184863
Helge Eichhorn <git@helgeeichhorn.de>
2016-11-04 22:56:57 +0100

    Add interpolation API and tests.

--- src/EarthOrientation.jl
+++ src/EarthOrientation.jl
@@ -1,6 +1,8 @@
 module EarthOrientation
 
-export EOPData
+import Base.Dates: datetime2julian, julian2datetime, Date, today, days
+
+export EOPData, interpolate
 
 const PATH = abspath(dirname(@__FILE__), "..", "data")
 
@@ -8,6 +10,16 @@ const NAMES = ("IAU1980", "IAU2000")
 const FILES = (joinpath(PATH, "finals.csv"), joinpath(PATH, "finals2000A.csv"))
 const URLS = ("https://datacenter.iers.org/eop/-/somos/5Rgv/latestXL/7/finals.all/csv",
               "https://datacenter.iers.org/eop/-/somos/5Rgv/latestXL/9/finals2000A.all/csv")
+
+const MJD_EPOCH = 2400000.5
+date(mjd) = Date(julian2datetime(mjd + MJD_EPOCH))
+
+type OutOfRangeError <: Base.Exception
+    mjd::Float64
+    when::String
+end
+Base.showerror(io::IO, e::OutOfRangeError) = print(io, "No data available ", when, " ", date(mjd))
+warn_extrapolation(mjd, when) = warn("No data available $when $(date(mjd)). Extrapolation is probably imprecise.")
 
 function update()
     !isdir(PATH) && mkdir(PATH)
@@ -24,18 +36,17 @@ end
 
 function getdate(data)
     idx = findlast(data[:,5] .== "final")
-    Base.Dates.Date(data[idx,2], data[idx,3], data[idx,4])
+    Date(data[idx,2], data[idx,3], data[idx,4])
 end
 
 function isold(file)
     data, header = readdlm(file, ';', header=true)
     timestamp = getdate(data)
-    today = Base.Dates.today()
-    Base.Dates.days(today - timestamp) > 7
+    days(today() - timestamp) > 7
 end
 
 type EOPData
-    date::Base.Dates.Date
+    date::Date
     mjd::Vector{Float64}
     xp::Vector{Nullable{Float64}}
     xp_err::Vector{Nullable{Float64}}
@@ -58,7 +69,7 @@ end
 
 function EOPData(iau1980::String, iau2000::String)
     data80, header80 = readdlm(iau1980, ';', header=true)
-    data00, header00 = readdlm(iau1980, ';', header=true)
+    data00, header00 = readdlm(iau2000, ';', header=true)
     n = size(data80, 1)
     date = getdate(data80)
     mjd = Vector{Float64}(data80[:,1])
@@ -69,11 +80,11 @@ function EOPData(iau1980::String, iau2000::String)
     ind1 = [collect(6:9); collect(11:14); collect(16:19)]
     ind2 = collect(20:23)
     for i = 1:n
-        row = view(data80, n, :)
+        row = view(data80, i, :)
         for (field, idx) in zip(fieldnames(EOPData)[3:end], ind1)
             getfield(eop, field)[i] = row[idx] == "" ? Nullable{Float64}() : Nullable(row[idx])
         end
-        row = view(data00, n, :)
+        row = view(data00, i, :)
         for (field, idx) in zip(fieldnames(EOPData)[end-3:end], ind2)
             getfield(eop, field)[i] = row[idx] == "" ? Nullable{Float64}() : Nullable(row[idx])
         end
@@ -83,5 +94,63 @@ end
 EOPData() = EOPData(FILES...)
 
 Base.show(io::IO, eop::EOPData) = print(io, "EOPData($(eop.date))")
+
+function interpolate(eop::EOPData, field::Symbol, jd::Float64; extrapolate=true, warnings=true)
+    interpolate(eop.mjd, getfield(eop, field), jd - MJD_EPOCH,
+                extrapolate=extrapolate, warnings=warnings)
+end
+
+function interpolate(eop::EOPData, field::Symbol, dt::DateTime; kwargs...)
+    interpolate(eop, field, datetime2julian(dt); kwargs...)
+end
+
+function interpolate(xv, yv, x; extrapolate=true, warnings=true)
+    idx = findfirst(map(y->isapprox(x, y), xv))
+    if idx != 0 && !isnull(yv[idx])
+        return get(yv[idx])
+    end
+    idx = findfirst(x .< xv)
+    if idx == 0
+        if extrapolate
+            warnings && warn_extrapolation(xv[end], "after")
+            idx = length(xv)
+        else
+            throw(OutOfRangeError(x, "after"))
+        end
+    elseif idx == 1
+        if extrapolate
+            warnings && warn_extrapolation(xv[1], "before")
+            idx = 2
+        else
+            throw(OutOfRangeError(x, "before"))
+        end
+    end
+    if isnull(yv[idx])
+        if extrapolate
+            idx = findlast(!isnull.(yv))
+            warnings && warn_extrapolation(xv[idx], "after")
+        else
+            throw(OutOfRangeError(x, "after"))
+        end
+    end
+    x0 = xv[idx-1]
+    y1 = get(yv[idx])
+    y0 = get(yv[idx-1])
+    y = y0 + (y1 - y0) * (x - x0)
+end
+
+for sym in fieldnames(EOPData)[3:end]
+    fun = Symbol(:get, sym)
+    s = Expr(:quote, sym)
+    @eval begin
+        function $fun(eop::EOPData, dt::DateTime; kwargs...)
+            interpolate(eop, $s, datetime2julian(dt); kwargs...)
+        end
+        function $fun(eop::EOPData, jd::Float64; kwargs...)
+            interpolate(eop, $s, jd; kwargs...)
+        end
+        export $fun
+    end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,46 @@
 using EarthOrientation
+import EarthOrientation.OutOfRangeError
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+@testset "EarthOrientation" begin
+    @testset "Interpolation" begin
+        xv = collect(0.0:3.0)
+        yv = [Nullable(el) for el in 0.0:2.0]
+        push!(yv, Nullable{Float64}())
+        @test interpolate(xv, yv, 1.0) == 1.0
+        @test interpolate(xv, yv, 1.5) == 1.5
+        @test interpolate(xv, yv, -1.0, warnings=false) == -1.0
+        @test interpolate(xv, yv, 3.0, warnings=false) == 3.0
+        @test interpolate(xv, yv, 30.0, warnings=false) == 30.0
+        @test_throws OutOfRangeError interpolate(xv, yv, -1.0, extrapolate=false)
+        @test_throws OutOfRangeError interpolate(xv, yv, 3.0, extrapolate=false)
+    end
+    @testset "API" begin
+        eop = EOPData()
+        dt = Base.Dates.DateTime(2000, 1, 1)
+        @test interpolate(eop, :δx, Base.Dates.datetime2julian(dt)) == -0.135
+        @test interpolate(eop, :δx, dt) == -0.135
+        @test getxp(eop, dt) == 0.043299
+        @test getxp_err(eop, dt) == 0.000092
+        @test getyp(eop, dt) == 0.377859
+        @test getyp_err(eop, dt) == 0.000099
+        @test getΔUT1(eop, dt) == 0.3554755
+        @test getΔUT1_err(eop, dt) == 0.0000099
+        @test getlod(eop, dt) == 0.9333
+        @test getlod_err(eop, dt) == 0.0076
+        @test getδψ(eop, dt) == -50.607
+        @test getδψ_err(eop, dt) == 0.791
+        @test getδϵ(eop, dt) == -2.585
+        @test getδϵ_err(eop, dt) == 0.298
+        @test getδx(eop, dt) == -0.135
+        @test getδx_err(eop, dt) == 0.315
+        @test getδy(eop, dt) == -0.204
+        @test getδy_err(eop, dt) == 0.298
+        dt = Base.Dates.DateTime(2000, 1, 1, 12)
+        @test getδx(eop, dt) ≈ -0.007
+        @test_throws OutOfRangeError getδx(eop, now() + Base.Dates.Year(1), extrapolate=false)
+        dt = Base.Dates.DateTime(1973, 1, 1)
+        @test_throws OutOfRangeError getδx(eop, dt, extrapolate=false)
+    end
+end
