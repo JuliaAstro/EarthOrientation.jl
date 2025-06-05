cfdffa48fc3595ff919ce7edf7c4c88a36fb4a6d
icweaver <weaveric@gmail.com>
2025-06-04 21:35:31 -0700

    updated links

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,14 @@ makedocs(
     modules = [EarthOrientation],
     authors = "Helge Eichhorn <git@helgeeichhorn.de>",
     sitename = "EarthOrientation.jl",
-    format = Documenter.HTML(),
-    pages = pages
+    format = Documenter.HTML(
+        canonical = "https://juliaastro.org/EarthOrientation/stable",
+    ),
+    pages = pages,
 )
 
 deploydocs(
     repo = "github.com/JuliaAstro/EarthOrientation.jl.git",
     push_preview = true,
+    versions = ["stable" => "v^", "v#.#"], # Restrict to minor releases
 )
