1a7ffd067489c61bb6eb53cdf34a0777d0d5e7a6
Ian Weaver <weaveric@gmail.com>
2025-06-06 15:28:04 -0700

    style

--- docs/make.jl
+++ docs/make.jl
@@ -2,17 +2,17 @@ using Documenter
 using EarthOrientation
 
 include("pages.jl")
-makedocs(
+makedocs(;
     modules = [EarthOrientation],
     authors = "Helge Eichhorn <git@helgeeichhorn.de>",
     sitename = "EarthOrientation.jl",
     format = Documenter.HTML(
-        canonical = "https://juliaastro.org/EarthOrientation/stable",
+        canonical = "https://juliaastro.org/EarthOrientation/stable/",
     ),
     pages = pages,
 )
 
-deploydocs(
+deploydocs(;
     repo = "github.com/JuliaAstro/EarthOrientation.jl.git",
     push_preview = true,
     versions = ["stable" => "v^", "v#.#"], # Restrict to minor releases
